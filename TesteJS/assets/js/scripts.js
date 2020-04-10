272299e3602f30a13064b70593f2318e8544851f
Jeferson Monteiro <jefimpjjn@gmail.com>
2020-04-09 21:47:43 -0300

    .

---
+++ TesteJS/assets/js/scripts.js
@@ -0,0 +1,17 @@
+//  function alerta(){
+    
+//     var alerta=prompt("")
+//     return alerta;
+//  }
+
+function atualizaNome(){
+  
+    var nome=prompt(" ");
+    nomeInicial.textContent="Olá " +nome;
+
+
+}
+function adicionar(){
+
+
+}
